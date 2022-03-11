9795f66f08879a0d93ff697aee104b74a18c5ba5
hugo-dutra <hugo.dutra@hotmail.com>
2022-03-11 16:16:59 -0300

    Adicionando pontos ao click

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import CanvasPoints from './CanvasPoints';
+import { useState } from 'react';
+import { drawCircle, clearCanvas } from '../services/CanvasService';
 
+const circles = [];
 
 const MainPanel = () => {
+  const handleCanvasClick = (e) => {
+    const { clientX, clientY } = e;
+    circles.push({ x: clientX, y: clientY });
+    circles.forEach(circle => {
+      drawCircle(circle.x, circle.y, 10);
+    })
+  }
 
   return (
-    <div>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <h1>MainPanel</h1>
-      <canvas id="panelCanvas"></canvas>
+      <canvas id="panelCanvas" onClick={handleCanvasClick} style={{ width: "100vw", height: "100vh" }} ></canvas>
     </div>
   )
 }
--- src/services/CanvasService.js
+++ src/services/CanvasService.js
@@ -1,19 +1,20 @@
-const drawCircle = (x, y, radius) => {
-  const panelCanvas = document.getElementById("panelCanvas");
+export const drawCircle = (x, y, radius) => {
+  var panelCanvas = document.getElementById("panelCanvas");
   panelCanvas.width = window.innerWidth;
   panelCanvas.height = window.innerHeight;
   const ctx = panelCanvas.getContext("2d");
+
   ctx.beginPath();
-  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.arc(x, y - panelCanvas.offsetTop, radius, 0, 2 * Math.PI);
   ctx.fillStyle = "red";
   ctx.fill();
   ctx.stroke();
 }
 
-const clearCanvas = () => {
+export const clearCanvas = () => {
   const panelCanvas = document.getElementById("panelCanvas");
   const ctx = panelCanvas.getContext("2d");
   ctx.clearRect(0, 0, panelCanvas.width, panelCanvas.height)
 }
 
-export drawCircle, clearCanvas;
+
