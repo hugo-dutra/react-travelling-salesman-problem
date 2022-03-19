750b8d0ffdc884b286fbcb8fb96c1755a415c08d
hugo-dutra <hugo.dutra@hotmail.com>
2022-03-19 11:54:44 -0300

    Adicionado axios e service e enviroment

---
+++ src/enums/hosts.js
@@ -0,0 +1,16 @@
+export const CURRENT_ENVIRIONMENT = ENVIRONMENTS.LOCAL;
+
+export const HOSTS = {
+  LOCAL_HOST: 'http://localhost:8080', //Can store .env
+  HOMOLOG_HOST: 'http://xxx.xxx.xxx.xxx:8080'
+  DEVELOP_HOST: 'http://xxx.xxx.xxx.xxx:8080'
+  PRODUCTION_HOST: 'http://xxx.xxx.xxx.xxx:8080'
+}
+
+export const ENVIRONMENTS = {
+  LOCAL: 'LOCAL',
+  HOMOLOG: 'HOMOLOG',
+  DEVELPOP: 'DEVELOP',
+  PRODUCTION: 'PRODUCTION'
+}
+
